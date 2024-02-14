19ea5dd36820eab1c7b0ae6c4d9e277e2b8dfcf6
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-02-14 05:49:16 +0330

    .

--- VSCode-Bot.js
+++ VSCode-Bot.js
@@ -7,7 +7,7 @@ let DinamicPercent = 0.001;
 
 
 const intervalId = setInterval(() => {
-  // console.clear();
+
   if (deposit <= 0) {
     clearInterval(intervalId);
     console.log("your liquaded");
