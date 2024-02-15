0602308b2972205ec2bfbd1dcbb212fe58bf6a09
m.hesam moradian <137858394+m-hesam-moradian@users.noreply.github.com>
2024-02-15 06:38:21 +0330

    .

--- VSCode-Bot.js
+++ VSCode-Bot.js
@@ -1,3 +1,41 @@
+////////////////////////////select number
+
+// async function getPrice() {
+//   try {
+//     let price = [
+//       ...document.querySelectorAll(".item-_gbYDtbd .itemTitle-_gbYDtbd"),
+//     ].find((item) => item.textContent === "Close");
+//     if (price) {
+//       //   console.log(price.nextElementSibling.textContent);
+//     } else {
+//       console.log("Price not found");
+//     }
+//     console.log("Current price: $" + price.nextElementSibling.textContent);
+//   } catch (error) {
+//     console.error("Failed to fetch price: " + error);
+//   } finally {
+//     setTimeout(getPrice, 1000);
+//   }
+
+// getPrice();
+///
+// async function Post(data) {
+//   const response = await fetch("https://mhm-jsonserver.liara.run/cryptoBot", {
+//     method: "POST",
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   });
+
+//   const resData = await response.json();
+//   return resData;
+// }
+
+// export default Post;
+
+/////////my bot
+
 let deposit = 10;
 let orderID = null;
 let trade = null;
@@ -5,9 +43,8 @@ let lastTrade = 1;
 let MonetFlowing = 0.1;
 let DinamicPercent = 0.001;
 
-
 const intervalId = setInterval(() => {
-
+  // console.clear();
   if (deposit <= 0) {
     clearInterval(intervalId);
     console.log("your liquaded");
@@ -45,7 +82,7 @@ const intervalId = setInterval(() => {
           "SL:",
           trade - trade * DinamicPercent
         );
-        DinamicPercent = 0.1;
+        DinamicPercent = 1;
       }
 
       if (GreenMA > RedMA) {
@@ -101,3 +138,49 @@ const intervalId = setInterval(() => {
   }
 }, 1000);
 
+// let deposit = 100;
+// let orderID = null;
+// let trade = null;
+// let lastTrade = 1;
+// let MonetFlowing = 1;
+
+// setInterval(() => {
+//   // console.clear();
+
+//   let price = [
+//     ...document.querySelectorAll(".item-_gbYDtbd .itemTitle-_gbYDtbd"),
+//   ].find((item) => item.textContent === "Close");
+
+//   if (price) {
+//     // console.log(price.nextElementSibling.textContent);
+//     if (trade) {
+//       if (
+//         Number(price.nextElementSibling.textContent) >=
+//         trade + trade * 0.0001
+//       ) {
+//         // console.log(deposit);
+//         deposit += MonetFlowing;
+//         MonetFlowing = 1;
+//         trade = null;
+//         console.log("reach to TP", deposit);
+//       } else if (
+//         Number(price.nextElementSibling.textContent) <=
+//         trade - trade * 0.0001
+//       ) {
+//         deposit -= MonetFlowing;
+//         console.log("reach to sl", deposit);
+//         // console.log(deposit);
+
+//         MonetFlowing *= 2;
+//         trade = null;
+//       }
+//     } else {
+//       // deposit;
+//       trade = Number(price.nextElementSibling.textContent);
+//       // console.log("make trade", deposit, trade);
+//     }
+//   } else {
+//     console.log("Price not found");
+//   }
+// }, 1000);
+////ai bot
